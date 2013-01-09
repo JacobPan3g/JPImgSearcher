e16fd0188c5e9a1637e28d402643f316679019be
JacobPan3g <zhenjian3g@qq.com>
2013-01-09 09:04:51 +0800

    结合前后端初步完成

--- view.py
+++ view.py
@@ -8,28 +8,29 @@ class BlockWindow( wx.StaticBitmap ):
 		bitmap = wx.Image( imgName, wx.BITMAP_TYPE_JPEG )
 		w = bitmap.GetWidth()
 		h = bitmap.GetHeight()
-		bitmap = bitmap.Scale( w/2, h/2 )
+		bitmap = bitmap.Scale( 128, 128 )
 #pos = ((192-w)/2,(192-h)/2)
 		pos = (100,100)
 		wx.StaticBitmap.__init__( self, parent, id, bitmap.ConvertToBitmap(), pos, size )
 		self.SetMinSize( size )
 		
 class GridSizerFrame( wx.Frame ):
-	def __init__( self ):
+	def __init__( self, imgNameArr ):
 		wx.Frame.__init__( self, None, -1, "Imager Searcher", size=(1366, 768) )
 		self.scroll = wx.ScrolledWindow( self, -1 )
 		self.scroll.SetScrollbars(1,1, 0, 0)
 		sizer = wx.GridSizer( rows=0, cols=7, hgap=3, vgap=3 )
 		
-		for i in xrange(100):
-			name = "images/%d.jpg" % i	
+		for imgName in imgNameArr:
+			name = "images/" + + ".jpg"	
 			bw = BlockWindow( self.scroll, imgName=name )
 			sizer.Add( bw, flag=wx.ALIGN_CENTER )
 		
 		self.scroll.SetSizer( sizer )
 		self.scroll.Fit()
 
-app = wx.PySimpleApp()
-GridSizerFrame().Show()
-app.MainLoop()
+def main( imgNameArr ):
+	app = wx.PySimpleApp()
+	GridSizerFrame( imgNameArr ).Show()
+	app.MainLoop()
 	
